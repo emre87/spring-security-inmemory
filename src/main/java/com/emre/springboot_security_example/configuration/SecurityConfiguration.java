ac81ac9ec96eda7e004ba90fade02f0ec45d54be
Şehmus Emre DEMİRALAY <sehmusemre.demiralay@tuikmerkez.tuik.gov.tr>
2022-12-28 12:05:48 +0300

    first load

---
+++ src/main/java/com/emre/springboot_security_example/configuration/SecurityConfiguration.java
@@ -0,0 +1,43 @@
+package com.emre.springboot_security_example.configuration;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    UserDetailsService userDetailsService;
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+        http.formLogin();
+        http.authorizeHttpRequests()
+                .antMatchers("/dashboard").hasAnyRole("ADMIN", "USER")
+                .antMatchers("/admin").hasRole("ADMIN")
+                .and()
+                .authorizeHttpRequests()
+                .antMatchers("/index")
+                .permitAll();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService);
+    }
+
+    //Deprecated olan bir metot ve artık kullanmamıza gerek yok noop derken yetiyor.
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+}
---
+++ src/main/java/com/emre/springboot_security_example/controller/UserController.java
@@ -0,0 +1,23 @@
+package com.emre.springboot_security_example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "Admine özel mesajdır";
+    }
+
+    @GetMapping("/dashboard")
+    public String  dashboard() {
+        return "Üyelere özel mesajdır";
+    }
+
+    @GetMapping("/index")
+    public String index(){
+        return "Merhaba. Üye Olun";
+    }
+}
